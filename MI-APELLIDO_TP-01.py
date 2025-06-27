2fc1c824c13b1737b2b6873f9cf59e7fdc84b2d8
Agustina Milagros Cruz <agustinamcruz-2013@hotmail.com>
2025-06-27 18:43:50 -0300

    Agreguo TP 01: Estructuras Secuenciales

---
+++ MI-APELLIDO_TP-01.py
@@ -0,0 +1,7 @@
+##TP 1 – Estructuras Secuenciales
+##Tecnicatura Universitaria en Programación a Distancia – UTN
+##Unidad 1 — Trabajo Práctico de Estructuras Secuenciales
+##Nombre: Agustina Milagros
+##Apellido: Cruz
+##Materia: Programación I
+##Año:2025
