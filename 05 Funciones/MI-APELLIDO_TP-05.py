4e00c67ee55e4acf9b9efb48edb4cdcab4fd39c5
Agustina Milagros Cruz <agustinamcruz-2013@hotmail.com>
2025-06-27 17:27:06 -0300

    Primer commit: TP 2 Funciones

---
+++ 05 Funciones/MI-APELLIDO_TP-05.py
@@ -0,0 +1,16 @@
+##TP 2 – Funciones
+##Tecnicatura Universitaria en Programación a Distancia – UTN
+##Unidad 5 — Trabajo Práctico de Funciones
+##Nombre: Agustina Milagros
+##Apellido: Cruz
+##Materia: Programación I
+##Año:2025
+
+
+##Objetivo del trabajo
+
+#El objetivo de este trabajo práctico es aprender a usar funciones en Python para organizar mejor
+#el código, hacerlo más fácil de entender, mantener y reutilizar.
+# A través de ejercicios prácticos, fui aplicando los conceptos fundamentales de las funciones
+# para resolver distintos problemas, desde cálculos matemáticos hasta transformaciones de datos.
+
