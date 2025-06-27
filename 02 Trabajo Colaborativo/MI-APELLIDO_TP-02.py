c9233be78b08df433080b528f42b3af0b4dc432c
Agustina Milagros Cruz <agustinamcruz-2013@hotmail.com>
2025-06-27 20:37:44 -0300

    Ajuste cambio en la descripcion del archivo.py

---
+++ 02 Trabajo Colaborativo/MI-APELLIDO_TP-02.py
@@ -0,0 +1,7 @@
+##TP 1– GIT
+##Tecnicatura Universitaria en Programación a Distancia – UTN
+##Unidad 2 — Trabajo Práctico de GIT
+##Nombre: Agustina Milagros
+##Apellido: Cruz
+##Materia: Programación I
+##Año:2025
