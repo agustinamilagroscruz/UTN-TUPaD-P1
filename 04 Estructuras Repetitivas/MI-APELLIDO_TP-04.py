a7cc8be8f55a28f49e8cfbf7187ffa3d71196306
Agustina Milagros Cruz <agustinamcruz-2013@hotmail.com>
2025-06-27 20:39:35 -0300

    Agregue cambios en el archivo.py

---
+++ 04 Estructuras Repetitivas/MI-APELLIDO_TP-04.py
@@ -0,0 +1,7 @@
+##TP 4 – Estructuras Repetitivas
+##Tecnicatura Universitaria en Programación a Distancia – UTN
+##Unidad 4— Trabajo Práctico de Estructuras Repetitivas
+##Nombre: Agustina Milagros
+##Apellido: Cruz
+##Materia: Programación I
+##Año:2025
