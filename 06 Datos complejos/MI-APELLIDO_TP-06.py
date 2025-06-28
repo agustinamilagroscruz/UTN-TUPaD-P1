59b5d5473033d5e692d501e6c4855e5b22264430
Agustina Milagros Cruz <agustinamcruz-2013@hotmail.com>
2025-06-27 22:48:22 -0300

    Actualizacion

---
+++ 06 Datos complejos/MI-APELLIDO_TP-06.py
@@ -0,0 +1,7 @@
+##TP 1 – Datos complejos
+##Tecnicatura Universitaria en Programación a Distancia – UTN
+##Unidad 7 — Trabajo Práctico de Datos Complejos
+##Nombre: Agustina Milagros
+##Apellido: Cruz
+##Materia: Programación I
+##Año:2025
